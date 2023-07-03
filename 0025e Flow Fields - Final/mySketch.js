ca2fecb07fa995f89e717e50e55d8282dc9fca98
Zackary Binx <59242014+Binxly@users.noreply.github.com>
2023-07-02 23:47:20 -0400

    more dumb readability changes

--- 0025e Flow Fields - Final/mySketch.js
+++ 0025e Flow Fields - Final/mySketch.js
@@ -17,7 +17,7 @@ let hueValue;
 
 function setup() {
     createCanvas(1024, 1280);
-    
+
     // columns and rows based on the scale
     cols = floor(width / scl);
     rows = floor(height / scl);
@@ -32,35 +32,34 @@ function setup() {
     colorMode(RGB, 255, 255, 255); // mode back to RGB
 
     background(0);
+
     // Stop the animation after 20 seconds.
     setTimeout(noLoop, 20000);
 }
 
 function setHueValue() {
-    // switching the color mode to HSL
+    // color mode to HSL.
     colorMode(HSL, 360, 100, 100);
 
-    // 10% chance of white, 90% chance of random color
-    if (random() < 0.1) {
-        hueValue = [0, 0, 100];  // HSL for white
-    } else {
-        hueValue = [floor(random() * 360), 100, 50];  // Random hue
-    }
+    // 10% chance of being white or a random color otherwise
+    hueValue = random() < 0.1 ? [0, 0, 100] : [floor(random() * 360), 100, 50];
 }
 
 function draw() {
     let yoff = 0;
+
     for (let y = 0; y < rows; y++) {
         let xoff = 0;
         for (let x = 0; x < cols; x++) {
             const index = x + y * cols;
             const angle = noise(xoff, yoff, zoff) * TWO_PI * 1.2; // * # -> higher, more variance. lower, less variation, more straight lines.
             const v = p5.Vector.fromAngle(angle);
+
             v.setMag(1); // higher faster, lower slower. changes how fast particles move across vectors by changing the magnitude (length) of the vectors)
             flowfield[index] = v;
             xoff += inc;
-            // Optional flow field vector lines rendering code commented
         }
+
         yoff += inc;
         zoff += 0.0003; // 0.0003 = faster, 0.0001 = slower (default is 0.0003, changes how fast the flow field vectors move (change direction) across the screen)
     }
@@ -76,5 +75,5 @@ function draw() {
 }
 
 function keyPressed() {
-    if (keyCode === 32) saveCanvas('myCanvas', 'png'); // press spacebar to save canvas as png
+    if (keyCode === 32) saveCanvas('FlowField', 'png'); // press spacebar to save canvas as png
 }
