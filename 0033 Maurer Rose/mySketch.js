977aeeb007d0774203eba3f35c1a7e69acfa14d7
Zackary Binx <59242014+Binxly@users.noreply.github.com>
2023-07-04 19:04:34 -0400

    Maurer Rose

--- 0033 Maurer Rose/mySketch.js
+++ 0033 Maurer Rose/mySketch.js
@@ -1,28 +1,55 @@
 //https://en.wikipedia.org/wiki/Maurer_rose
 //https://www.youtube.com/watch?v=4uU9lZ-HSqA - Maurer Rose
 
-let n = 6;
-let d = 71;
+let n = 2;
+let d = 29;
+let dSlider;
 
 function setup() {
     createCanvas(700, 700);
     angleMode(DEGREES);
-	background(0);
+    //dSlider = createSlider(1, 180, 1);
 	
 }
 
 function draw() {
+    background(0);
     translate(width / 2, height / 2);
     stroke(255);
 
+    //d = dSlider.value();
+
     noFill();
+    strokeWeight(1);
     beginShape();
     for (let i = 0; i < 361; i++) {
-        let k = i*d;
+        let k = i * d;
+        let r = 300 * sin(n * k);
+        let x = r * cos(k);
+        let y = r * sin(k);
+        vertex(x,y);
+    }
+    endShape(CLOSE);
+
+    noFill();
+    stroke(255,0,0, 100);
+    strokeWeight(4);
+    beginShape();
+    for (let i = 0; i < 361; i++) {
+        let k = i;
         let r = 300 * sin(n*k);
         let x = r * cos(k);
         let y = r * sin(k);
         vertex(x,y);
     }
     endShape(CLOSE);
+    
+    // n += 0.001;
+    // d += 0.0118333333;
+    // if (n > 6) {
+    //     n = 6;
+    // }
+    // if (d > 71) {
+    //     d = 71;
+    // }
 }
