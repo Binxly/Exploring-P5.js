56c01bc80690b81b2d2592a3db98ad61f6b7dc12
Zackary Binx <59242014+Binxly@users.noreply.github.com>
2023-07-04 19:06:28 -0400

    Template files for easier setup of new projects.

---
+++ 0000 TEMPLATE/mySketch.js
@@ -0,0 +1,15 @@
+function setup() {
+    createCanvas(700, 700);
+	
+}
+
+function draw() {
+    background(0); // black background
+    translate(width / 2, height / 2); // sets origin to center of canvas
+
+    stroke(255);
+    noFill();
+    strokeWeight(1);
+    
+    
+}
