5743d1e154192c3db2908861340c447a3fafc163
Zackary Binx <59242014+Binxly@users.noreply.github.com>
2023-07-04 01:22:48 -0400

    idk

--- 0029 tiling shapes/mySketch.js
+++ 0029 tiling shapes/mySketch.js
@@ -17,16 +17,14 @@ function draw() {
     stroke(lineColor)
     noFill();
     strokeWeight(2);
-    // Loop through the width and height of the canvas, incrementing by the tile size
   for(let x = 0; x < width; x += tileSize) {
     for(let y = 0; y < height; y += tileSize) {
-      // Draw two lines to form the 'V' shape of the herringbone pattern
-      if ((x / tileSize) % 2 == (y / tileSize) % 2) {  // Check if the tile is even or odd
-        line(x, y, x + tileSize, y + tileSize);  // Draw the line from the top left to bottom right of the tile
-        line(x, y + tileSize, x + tileSize, y);  // Draw the line from the bottom left to top right of the tile
+      if ((x / tileSize) % 2 == (y / tileSize) % 2) {
+        line(x, y, x + tileSize, y + tileSize);
+        line(x, y + tileSize, x + tileSize, y);
       } else {
-        line(x, y, x + tileSize, y + tileSize);  // Draw the line from the top left to bottom right of the tile
-        line(x + tileSize, y, x, y + tileSize);  // Draw the line from the top right to bottom left of the tile
+        line(x, y, x + tileSize, y + tileSize);
+        line(x + tileSize, y, x, y + tileSize);
       }
     }
   }
